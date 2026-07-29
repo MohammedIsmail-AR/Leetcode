883917cd855fce6a90fafdbca56b65d7215b9445
Mohammed Ismail <mohammedismail3220@gmail.com>
2026-07-29 19:24:24 +0530

    Time: 12 ms (83.37%), Space: 14.3 MB (9.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,8 @@
 class Solution(object):
     def isPalindrome(self, s):
-        l = 0
-        r =len(s)-1
+        processed_s = "".join([x.lower() for x in s if x.isalpha() or x.isdigit()])
+        return processed_s == processed_s[::-1]
+        
 
-        while l < r :
-            while l < r and not self.alphanum (s[l]):
-                l += 1
-            while r > l and not self.alphanum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l,r = l+1, r-1   
-        return True 
-
-
-    def alphanum(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or 
-                ord('a') <= ord(c) <= ord('z') or 
-                ord('0') <= ord(c) <= ord('9')) 
-    
 
 
