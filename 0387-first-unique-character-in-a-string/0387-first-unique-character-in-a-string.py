b26431f60a62563ad06c6b7d91d590fbbcfb1230
Mohammed Ismail <mohammedismail3220@gmail.com>
2026-08-06 18:34:47 +0530

    Time: 124 ms (25.04%), Space: 12.8 MB (60.6%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        freq=[0]*26
+        for c in s:
+            freq[ord(c)-ord('a')]+=1
+        for i,c in enumerate(s):
+            if freq[ord(c)-ord('a')]==1:
+                return i
+        return -1
+
+        
+
+
+        
+
+        
+       
+
+
+        
