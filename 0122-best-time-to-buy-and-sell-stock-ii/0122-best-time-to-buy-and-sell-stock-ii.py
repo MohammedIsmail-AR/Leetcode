e007b2c7a7720cfefcdedf8e336fd1aea10a4106
Mohammed Ismail <mohammedismail3220@gmail.com>
2026-08-25 17:41:33 +0530

    Time: 4 ms (40.83%), Space: 13.8 MB (37%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -4,9 +4,11 @@ class Solution(object):
 
         for i in range(1,len(prices)):
             if prices[i]>prices[i-1]:
-                profit += (prices[i]- prices[i-1])
+                profit += prices[i]- prices[i-1]
 
         return profit
+
+
   
 
 
