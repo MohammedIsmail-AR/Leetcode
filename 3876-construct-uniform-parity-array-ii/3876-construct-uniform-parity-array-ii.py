b6a983167159a4f7aadb24113869ae67f8a9f54d
Mohammed Ismail <mohammedismail3220@gmail.com>
2026-09-03 20:27:25 +0530

    Time: 74 ms (50%), Space: 35.4 MB (51.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        smallestOdd = float('inf')
+
+        for num in nums1:
+            if num % 2 == 1:
+                smallestOdd = min(smallestOdd, num)
+
+        # Already all even
+        if smallestOdd == float('inf'):
+            return True
+
+        # Check whether every even number can become odd
+        for num in nums1:
+            if num % 2 == 0 and num <= smallestOdd:
+                return False
+
+        return True
+        
